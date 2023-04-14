a56738ad2a3d31e6adacc554cd44717b27505458
Tina Chen <tinachen@GinnywudeMacBook-Pro.local>
2023-04-14 11:04:02 +0800

    doc: start

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}","*.html","./js/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom__primary': '#B3CC59',
+        'custom__secondary': '#E6EBD0',
+        'custom__third': '#FFF8DC',
+        'custom__alert': '#D02424',
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      minHeight: {
+        '1/2': '50%',
+      },
+      
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
