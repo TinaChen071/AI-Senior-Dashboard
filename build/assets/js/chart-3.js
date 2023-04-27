75a864b0fd80090199a8a6a1f4f9905ffbc7c7aa
Tina Chen <tinachen@GinnywudeMacBook-Pro.local>
2023-04-27 16:44:24 +0800

    style: chart bar style

--- build/assets/js/chart-3.js
+++ build/assets/js/chart-3.js
@@ -5,12 +5,13 @@ var ctx = document.getElementById("chart-bars-3").getContext("2d");
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['一月', '二月', '三月'],
+    labels: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
     datasets: [{
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)'
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(255, 99, 132, 0.2)'
       ],
       borderColor: [
         'rgba(255,99,132,1)',
@@ -19,8 +20,8 @@ new Chart(ctx, {
         'rgba(75, 192, 192, 1)'
       ],
       borderWidth: 1,
-      label: '銷售業績(百萬)',
-      data: [60, 49, 72]
+      label: '人數',
+      data: [60, 49, 72, 100]
     }]
   }
 });
